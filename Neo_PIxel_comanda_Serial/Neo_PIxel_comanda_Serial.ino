085967fefa4d7dcb9671eae49f466f227fd93d22
GeorgeAlex990 <104625322+GeorgeAlex990@users.noreply.github.com>
2023-09-06 11:27:38 +0300

    Add files via upload

---
+++ Neo_PIxel_comanda_Serial/Neo_PIxel_comanda_Serial.ino
@@ -0,0 +1,40 @@
+#include <Adafruit_NeoPixel.h>
+
+#define Pin 9
+#define NumarPixeli 8
+
+Adafruit_NeoPixel pixels(NumarPixeli, Pin, NEO_RGB + NEO_KHZ800);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  pixels.begin();
+}
+
+int rosu = 0;
+int verde = 0;
+int albastru = 0;
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial.available()){
+    pixels.clear();
+    String DataIn = Serial.readString();
+    Serial.print("Rosu: ");
+    rosu = DataIn.substring(0, 3).toInt();
+    Serial.println(rosu);
+    Serial.print("Verde: ");
+    verde = DataIn.substring(3, 6).toInt();
+    Serial.println(verde);
+    Serial.print("Albastru: ");
+    albastru = DataIn.substring(6, 9).toInt();
+    Serial.println(albastru);
+
+    for (int foo = 0; foo <= NumarPixeli; ++foo) {
+      pixels.setPixelColor(foo, pixels.Color(verde, rosu, albastru));
+    }
+
+    pixels.show();
+  }
+}
